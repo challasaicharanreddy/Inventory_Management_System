2768f0f37be92c96a59c564d381baaf62853a1d3
challasaicharanreddy <challasaicharan3@gmail.com>
2026-07-07 15:36:40 +0530

    After frontend deploy

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -21,7 +21,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://YOUR-APP.vercel.app",
+            "https://inventory-management-system-delta-ochre.vercel.app/",
         ],
         credentials: true,
     })
